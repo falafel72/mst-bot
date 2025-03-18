10d331e299febe358e2a19709093fee532c2c00d
Matei-Alexandru Gardus <matei@gard.us>
2025-03-17 23:14:58 -0700

    Setup Nix flake to build Rust binaries
    This flake is capable of compiling x86_64 and ARM64 binaries across any
    default system that supports cross-compilation.

--- flake.nix
+++ flake.nix
@@ -9,91 +9,104 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { nixpkgs, crane, flake-utils, rust-overlay, ... }:
-    flake-utils.lib.eachDefaultSystem (localSystem:
-      let
-        systemsToBuildFor = ["x86_64-linux" "aarch64-linux"];
-        # Attribute set of all targets to compile for.
-        # This will be referenced in outputs when we define what target we want.
-        # targetPkgs = lib.attrsets.genAttrs systemsToBuildFor (target:
-        #   import nixpkgs {
-        #     crossSystem = target;
-        #     inherit localSystem;
-        #     overlays = [ (import rust-overlay ) ];
-        #   };
-        # );
+  outputs = {
+    self,
+    nixpkgs,
+    crane,
+    flake-utils,
+    rust-overlay,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      localSystem: let
+        systemsToBuildFor = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
         pkgs = import nixpkgs {
           inherit localSystem;
-          crossSystem = "x86_64-linux";
-          overlays = [ (import rust-overlay ) ];
+          overlays = [(import rust-overlay)];
         };
-
         # Pin Rust toolchain via overlay.
         rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        inherit (pkgs) lib;
 
         # Setup Crane functions with pinned toolchain in project.
         craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
 
-        # Separate out the migrations to use later
-        # for the SQLx CLI setup.
-        unfilteredRoot = ./.;
-        src = lib.fileset.toSource {
-          root = unfilteredRoot;
-          fileset = lib.fileset.unions [
-            (craneLib.fileset.commonCargoSources unfilteredRoot)
-            ./migrations
+        # Function to generate the relevant Crane package for the system target selected.
+        #
+        # This lets us eventually generate one crane package for each cross-compilation build
+        # we're doing.
+        generateCranePackage = (
+          crossSystem: let
+            pkgs = import nixpkgs {
+              inherit localSystem crossSystem;
+              overlays = [(import rust-overlay)];
+            };
+            inherit (pkgs) lib;
+
+            # Setup Crane functions with pinned toolchain in project.
+            craneLib = (crane.mkLib pkgs).overrideToolchain (p: p.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml);
+
+            # Separate out the migrations to use later
+            # for the SQLx CLI setup.
+            unfilteredRoot = ./.;
+            src = lib.fileset.toSource {
+              root = unfilteredRoot;
+              fileset = lib.fileset.unions [
+                (craneLib.fileset.commonCargoSources unfilteredRoot)
+                ./migrations
+              ];
+            };
+
+            commonArgs = {
+              inherit src;
+              strictDeps = true;
+              nativeBuildInputs = with pkgs.pkgsBuildHost; [
+                pkg-config
+              ];
+              buildInputs = with pkgs.pkgsHostHost;
+                [
+                  openssl
+                ]
+                ++ lib.optionals stdenv.isDarwin [
+                  libiconv
+                  darwin.apple_sdk.frameworks.Security
+                ];
+            };
+            cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+          in
+            craneLib.buildPackage (commonArgs
+              // {
+                inherit cargoArtifacts;
+                nativeBuildInputs =
+                  (commonArgs.nativeBuildInputs or [])
+                  ++ [
+                    pkgs.pkgsBuildHost.sqlx-cli
+                  ];
+                preBuild = ''
+                  export DATABASE_URL=sqlite:./database.db
+                  sqlx database create
+                  sqlx migrate run
+                '';
+              })
+        );
+        cranePackages = lib.attrsets.genAttrs systemsToBuildFor (system: generateCranePackage system);
+      in {
+        checks = cranePackages // {
+          default = cranePackages."${localSystem}";
+        };
+
+        packages = cranePackages // {
+          default = cranePackages."${localSystem}";
+        };
+
+        devShells.default = craneLib.devShell {
+          checks = self.checks.${localSystem};
+          packages = with pkgs; [
+            sqlx-cli
+            ripgrep
+            fd
           ];
         };
-
-        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
-        mst-bot-crate = craneLib.buildPackage (commonArgs // {
-          inherit cargoArtifacts;
-          nativeBuildInputs = (commonArgs.nativeBuildInputs ++ [
-            pkgs.sqlx-cli
-          ]);
-          pre-build = ''
-            export DATABASE_URL=sqlite:./database.db
-            sqlx database create
-            sqlx migrate run
-          '';
-        });
-
-
-        crateExpression =
-        {
-            openssl,
-            darwin,
-            libiconv,
-            lib,
-            pkg-config,
-            stdenv,
-        }:
-        let
-          commonArgs = {
-            inherit src;
-            strictDeps = true;
-
-            nativeBuildInputs = [
-              pkg-config
-            ];
-
-            buildInputs = [
-              openssl
-            ] ++ lib.optionals pkgs.stdenv.isDarwin [
-              # Hacky macOS dependencies. Wonderful.
-              libiconv
-              darwin.apple_sdk.frameworks.Security
-            ];
-          };
-        in
-        craneLib.buildPackage {
-          inherit src;
-          strictDeps = true;
-          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
-          nativeBuildInputs = [ pkg-config ] ++ lib.optionals stdenv.buildPlatform.isDarwin [ libiconv ];
-          buildInputs = [ openssl ];
-        };
-        
-      in 
-    )
+      }
+    );
 }
